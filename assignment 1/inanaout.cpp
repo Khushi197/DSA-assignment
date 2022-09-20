6129308d856088a1d32e17b091be50daa219494d
Khushi197 <113971416+Khushi197@users.noreply.github.com>
2022-09-20 15:56:22 +0530

    Create inanaout.cpp

---
+++ assignment 1/inanaout.cpp
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true#include <cmath>
+
+
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int s=a+b+c;
+    cout<<s;
+    return 0;
+}
